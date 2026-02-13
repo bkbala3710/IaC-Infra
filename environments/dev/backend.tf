a9690d8cd49e11bd72fa75db29a6be4d88f34083
Balakumaran T <bkbala3710@gmail.com>
2026-02-13 12:43:37 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+  backend "s3" {
+    bucket = "12112025"
+    key    = "environments/dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "12112025dy" 
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
