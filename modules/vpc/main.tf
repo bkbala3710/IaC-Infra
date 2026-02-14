414015f44d9daca38dc9a51dfcc8fb410764571b
Balakumaran T <bkbala3710@gmail.com>
2026-02-14 15:29:02 +0000

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "myvpc_4" {
+resource "aws_vpc" "myvpc_1" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
@@ -7,7 +7,7 @@ resource "aws_vpc" "myvpc_4" {
   }
 }
   
-resource "aws_subnet" "PuA_4" {
+resource "aws_subnet" "PuA_1" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = var.subnet_cidr_PuA
   availability_zone = var.subnet_az_PuA
@@ -17,7 +17,7 @@ resource "aws_subnet" "PuA_4" {
   }
 }
 
-resource "aws_subnet" "PuB_4" {
+resource "aws_subnet" "PuB_1" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = var.subnet_cidr_PuB
   availability_zone = var.subnet_az_PuB
@@ -28,7 +28,7 @@ resource "aws_subnet" "PuB_4" {
 }
 
 output "subnet_id-PuA" {
-  value       = aws_subnet.PuA_4.id
+  value       = aws_subnet.PuA_1.id
   description = "The Subnet which my EC2 will be created"
 }
 output "subnet_id_PuB" {
