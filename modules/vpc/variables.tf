820c82165449c528cd59f3422efc51c94539d431
Balakumaran T <bkbala3710@gmail.com>
2026-02-14 15:24:54 +0000

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,39 +1,39 @@
-variable "vpc_cidr" {
+variable "vpc_cidr_4" {
   type        = string
   description = "VPC CIDR"
 }
 
-variable "subnet_cidr_PuA" {
+variable "subnet_cidr_PuA_4" {
   type        = string
   description = "SUBNET CIDR"
 }
 
-variable "subnet_cidr_PuB" {
+variable "subnet_cidr_PuB_4" {
   type        = string
   description = "SUBNET CIDR"
 }
 
-variable "subnet_az_PuA" {
+variable "subnet_az_PuA_4" {
   type        = string
   description = "AZ of the Subnet"
 }
 
-variable "subnet_az_PuB" {
+variable "subnet_az_PuB_4" {
   type        = string
   description = "AZ of the Subnet"
 }
 
-variable "env" {
+variable "env_4" {
   type        = string
   description = "What Env we are creating (dev, qa, production)"
 }
 
-variable "PuA" {
+variable "PuA_4" {
   type        = string
   description = "What Env we are creating (dev, qa, production)"
 }
 
-variable "PuB" {
+variable "PuB_4" {
   type        = string
   description = "What Env we are creating (dev, qa, production)"
 }
