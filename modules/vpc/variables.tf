30a9f19904bf33cc0f267d7fc0bd9bd1f68e8afe
Balakumaran T <bkbala3710@gmail.com>
2026-02-14 15:14:03 +0000

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,70 @@
+Skip to content
+bkbala3710
+IaC-Infra
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+IaC-Infra/modules/vpc
+/variables.tf
+Go to file
+t
+bkbala3710
+bkbala3710
+Update variables.tf
+6e77544
+ · 
+now
+IaC-Infra/modules/vpc
+/variables.tf
+
+Code
+
+Blame
 variable "env" {
-    type        = string
-    description = "type of environment" 
+  type        = string
+  description = "What Env we are creating (dev, qa, production)"
+}
+
+variable "PuA" {
+  type        = string
+  description = "What Env we are creating (dev, qa, production)"
+}
+
+variable "PuB" {
+  type        = string
+  description = "What Env we are creating (dev, qa, production)"
 }
 
 variable "vpc_cidr" {
-    type        = string
-    description = "cdir of vpc" 
+  type        = string
+  description = "VPC CIDR"
 }
 
-variable "subnet_cidr" {
-    type        = string
-    description = "cdir of subnet" 
+variable "subnet_cidr_PuA" {
+  type        = string
+  description = "SUBNET CIDR"
 }
 
-variable "subnet_az"{
-    type        = string
-    description = "availability zone" 
+variable "subnet_cidr_PuB" {
+  type        = string
+  description = "SUBNET CIDR"
 }
+
+variable "subnet_az_PuA" {
+  type        = string
+  description = "AZ of the Subnet"
+}
+
+variable "subnet_az_PuB" {
+  type        = string
+  description = "AZ of the Subnet"
+}
+ 
