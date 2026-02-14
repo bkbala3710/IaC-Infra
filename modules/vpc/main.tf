cdd92e0a41707f073cd1e77ae62f74d6334b4547
Balakumaran T <bkbala3710@gmail.com>
2026-02-14 14:43:42 +0000

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,6 +17,24 @@ resource "aws_subnet" "main" {
   }
 }
 
+---
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id            = var.vpc_id
+  availability_zone = var.azs[count.index]
+
+  # Convert /16 → /24
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 1)
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+---
+
 output "subnet_id" {
   value       = aws_subnet.main.id
   description = "The Subnet which my EC2 will be created"
