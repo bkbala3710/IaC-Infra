612bfc09044b3ad20e2640ce4d9299ee245b7d70
Balakumaran T <bkbala3710@gmail.com>
2026-02-14 15:20:35 +0000

    Update main.tf

--- environments/main/main.tf
+++ environments/main/main.tf
@@ -1,9 +1,9 @@
 
 module "vpc" {
   source        = "../../modules/vpc"
-  vpc_cidr      = "10.0.0.0/16"
-  subnet_cidr_PuA   = "10.0.1.0/24"
-  subnet_cidr_PuB   = "10.0.2.0/24"
+  vpc_cidr      = "10.3.0.0/16"
+  subnet_cidr_PuA   = "10.3.1.0/24"
+  subnet_cidr_PuB   = "10.3.2.0/24"
   subnet_az_PuA     = "${var.aws_region}a"          # ap-southeast-1a
   subnet_az_PuB     = "${var.aws_region}b"          # ap-southeast-1b
   env           = "UW3"
