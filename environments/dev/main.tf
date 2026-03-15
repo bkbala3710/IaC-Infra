1190f68db4e51ff6dbcd4fd1f19c03ed5c71ac24
Balakumaran T <bkbala3710@gmail.com>
2026-03-15 15:48:22 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,15 +6,27 @@ module "vpc" {
   subnet_cidr_PuB   = "10.0.2.0/24"
   subnet_cidr_PrA   = "10.0.11.0/24"
   subnet_cidr_PrB   = "10.0.12.0/24"
+  subnet_cidr_PrC   = "10.0.21.0/24"
+  subnet_cidr_PrD   = "10.0.22.0/24"
+  subnet_cidr_PrE   = "10.0.31.0/24"
+  subnet_cidr_PrF   = "10.0.32.0/24"
   subnet_az_PuA     = "${var.aws_region}a"          # ap-southeast-1a
   subnet_az_PuB     = "${var.aws_region}b"          # ap-southeast-1b
   subnet_az_PrA     = "${var.aws_region}a"          # ap-southeast-1a
   subnet_az_PrB     = "${var.aws_region}b"          # ap-southeast-1b
+  subnet_az_PrC     = "${var.aws_region}a"          # ap-southeast-1a
+  subnet_az_PrD     = "${var.aws_region}b"          # ap-southeast-1b
+  subnet_az_PrE     = "${var.aws_region}a"          # ap-southeast-1a
+  subnet_az_PrF     = "${var.aws_region}b"          # ap-southeast-1b
   env           = "UW2"
   PuA           = "UW2-PUBLIC-1"
   PuB           = "UW2-PUBLIC-2"
   PrA           = "UW2-PRIVATE-1"
   PrB           = "UW2-PRIVATE-2"
+  PrC           = "UW2-PRIVATE-3"
+  PrD           = "UW2-PRIVATE-4"
+  PrE           = "UW2-PRIVATE-5"
+  PrF           = "UW2-PRIVATE-6"
 }
 
 /*
