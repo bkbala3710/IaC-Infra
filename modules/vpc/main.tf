9cff82ed2ae8fab5133bada65118d2d64a07b53a
Balakumaran T <bkbala3710@gmail.com>
2026-03-15 13:57:08 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -27,11 +27,40 @@ resource "aws_subnet" "PuB" {
   }
 }
 
+resource "aws_subnet" "PrA" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = var.subnet_cidr_PrA
+  availability_zone = var.subnet_az_PrA
+
+  tags = {
+    Name = "${var.PrA}-SUBNET"
+  }
+}
+
+resource "aws_subnet" "PrB" {
+  vpc_id            = aws_vpc.myvpc.id
+  cidr_block        = var.subnet_cidr_PrB
+  availability_zone = var.subnet_az_PrB
+
+  tags = {
+    Name = "${var.PrB}-SUBNET"
+  }
+}
+
 output "subnet_id-PuA" {
   value       = aws_subnet.PuA.id
   description = "The Subnet which my EC2 will be created"
 }
 output "subnet_id_PuB" {
   value       = aws_subnet.PuB.id
+  description = "The Subnet which my EC2 will be created"
+}
+
+output "subnet_id-PrA" {
+  value       = aws_subnet.PrA.id
+  description = "The Subnet which my EC2 will be created"
+}
+output "subnet_id_PrB" {
+  value       = aws_subnet.PrB.id
   description = "The Subnet which my EC2 will be created"
 }
