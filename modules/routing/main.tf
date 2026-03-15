7a4b1b7919b5de51bcbc5ca1d00a3c2a9e4ca928
Balakumaran T <bkbala3710@gmail.com>
2026-03-15 17:23:51 +0530

    Update main.tf

--- modules/routing/main.tf
+++ modules/routing/main.tf
@@ -92,27 +92,27 @@ resource "aws_route" "nat_route_az2" {
   nat_gateway_id         = aws_nat_gateway.nat_az2.id
 }
 ##################associate private subnets##########################
-resource "aws_route_table_association" "private_az1" {
+resource "aws_route_table_association" "private_subnet_1" {
   subnet_id      = var.subnet_id_PrA
   route_table_id = aws_route_table.private_rt_az1.id
 }
-resource "aws_route_table_association" "private_az2" {
+resource "aws_route_table_association" "private_subnet_2" {
   subnet_id      = var.subnet_id_PrB
   route_table_id = aws_route_table.private_rt_az2.id
 }
-resource "aws_route_table_association" "private_az1" {
+resource "aws_route_table_association" "private_subnet_3" {
   subnet_id      = var.subnet_id_PrC
   route_table_id = aws_route_table.private_rt_az1.id
 }
-resource "aws_route_table_association" "private_az2" {
+resource "aws_route_table_association" "private_subnet_4" {
   subnet_id      = var.subnet_id_PrD
   route_table_id = aws_route_table.private_rt_az2.id
 }
-resource "aws_route_table_association" "private_az1" {
+resource "aws_route_table_association" "private_subnet_5" {
   subnet_id      = var.subnet_id_PrE
   route_table_id = aws_route_table.private_rt_az1.id
 }
-resource "aws_route_table_association" "private_az2" {
+resource "aws_route_table_association" "private_subnet_6" {
   subnet_id      = var.subnet_id_PrF
   route_table_id = aws_route_table.private_rt_az2.id
 }
