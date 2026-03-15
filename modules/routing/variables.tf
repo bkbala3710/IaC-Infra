ffd1e640818ca26ab2b76390e25bccce9dec7036
Balakumaran T <bkbala3710@gmail.com>
2026-03-15 17:17:48 +0530

    Update variables.tf

--- modules/routing/variables.tf
+++ modules/routing/variables.tf
@@ -14,3 +14,26 @@ variable "subnet_id_PuA" {
 variable "subnet_id_PuB" {
   type = string
 }
+
+variable "subnet_id_PrA" {
+  type = string
+}
+
+variable "subnet_id_PrB" {
+  type = string
+}
+
+variable "subnet_id_PrC" {
+  type = string
+}
+
+variable "subnet_id_PrD" {
+  type = string
+}
+variable "subnet_id_PrE" {
+  type = string
+}
+
+variable "subnet_id_PrF" {
+  type = string
+}
