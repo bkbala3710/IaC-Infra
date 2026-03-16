72e7d692f78dc97dfa91b6962650205ca3e46a73
Balakumaran T <bkbala3710@gmail.com>
2026-03-16 15:09:05 +0530

    Update main.tf

--- modules/alb_asg/main.tf
+++ modules/alb_asg/main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_template" {
   image_id      = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [aws_security_group.presentation_ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.presentation_ec2_sg_id.id]
 
   user_data = base64encode(<<EOF
 #!/bin/bash
