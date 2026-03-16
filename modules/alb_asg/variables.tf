9221598ffbf9fa92cef49b257b79de6599492ecd
Balakumaran T <bkbala3710@gmail.com>
2026-03-16 15:36:26 +0530

    Update variables.tf

--- modules/alb_asg/variables.tf
+++ modules/alb_asg/variables.tf
@@ -1,6 +1,9 @@
 variable "presentation_ec2_sg_id" {
   type = string
 }
+variable "alb_sg_id" {
+  type = string
+}
 variable "env" {
   type        = string
   description = "What Env we are creating (dev, qa, production)"
