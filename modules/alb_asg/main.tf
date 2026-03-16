41ab14202bc569fb91158b42961d0f31da6cc700
Balakumaran T <bkbala3710@gmail.com>
2026-03-16 15:03:19 +0530

    Update main.tf

--- modules/alb_asg/main.tf
+++ modules/alb_asg/main.tf
@@ -1,1 +1,16 @@
+resource "aws_launch_template" "web_template" {
+  name_prefix   = "web-template"
+  image_id      = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 
+  vpc_security_group_ids = [aws_security_group.presentation_ec2_sg.id]
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "Welcome from ASG instance" > /var/www/html/index.html
+EOF
+  )
+}
