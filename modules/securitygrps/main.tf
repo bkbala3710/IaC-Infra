cdfaa73d5693fea400c52d829ab1e25aab9483b6
Balakumaran T <bkbala3710@gmail.com>
2026-03-16 14:55:55 +0530

    Update main.tf

--- modules/securitygrps/main.tf
+++ modules/securitygrps/main.tf
@@ -16,7 +16,26 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+################### Bastion host ###########################
+resource "aws_security_group" "bastion_host" {
+  name   = "bastion_host"
+  vpc_id = var.vpc_id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+################### Presentation layer sg ###########################
 resource "aws_security_group" "presentation_ec2_sg" {
   name   = "presentation_ec2_sg"
   vpc_id = var.vpc_id
@@ -32,7 +51,7 @@ resource "aws_security_group" "presentation_ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_IP/32"]
+    security_groups = [aws_security_group.bastion_host.id]
   }
 
   egress {
